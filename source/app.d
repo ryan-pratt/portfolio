3ef251a04e60b74a283325217e4c751f85ef621f
Ryan Pratt <ryan.pratt@live.com>
2019-09-02 21:07:37 -0500

    add router

--- source/app.d
+++ source/app.d
@@ -5,13 +5,16 @@ void main()
 	auto settings = new HTTPServerSettings;
 	settings.port = 8080;
 	settings.bindAddresses = ["::1", "127.0.0.1"];
-	listenHTTP(settings, &hello);
 
-	logInfo("Please open http://127.0.0.1:8080/ in your browser.");
+	auto router = new URLRouter;
+	router.get("/", &index);
+
+	listenHTTP(settings, router);
+
 	runApplication();
 }
 
-void hello(HTTPServerRequest req, HTTPServerResponse res)
+void index(HTTPServerRequest req, HTTPServerResponse res)
 {
 	res.writeBody("Hello, World!");
 }
